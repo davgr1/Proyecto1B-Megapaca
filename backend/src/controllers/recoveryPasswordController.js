930cce6603421f581880ff5e1fcf9481a6d07ba2
Gaby <20240153@ricaldone.edu.sv>
2026-04-28 14:02:47 -0600

    recuperar contraseña terminado

--- backend/src/controllers/recoveryPasswordController.js
+++ backend/src/controllers/recoveryPasswordController.js
@@ -1,4 +1,4 @@
-import  JsonWebtoken  from "jsonwebtoken";
+import  jsonwebtoken  from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
@@ -8,7 +8,6 @@ import HTMLRecoveryEmail from "../utils/sendMailRecovery.js";
 import { config } from "../../config.js";
 
 import customerModel from "../models/customers.js";
-import { json } from "stream/consumers";
 
 const recoveryPasswordController = {};
 
@@ -27,7 +26,7 @@ recoveryPasswordController.requestCode = async (req, res) => {
         const randomCode = crypto.randomBytes(3).toString("hex")
 
         //guardamos todo en un tpken
-        const token = JsonWebtoken.sign(
+        const token = jsonwebtoken.sign(
             //#1 que guardaremos
             {email, randomCode, userType: "customer", verified: false},
             //#2 clave secreta
@@ -76,72 +75,50 @@ recoveryPasswordController.requestCode = async (req, res) => {
     }
 };                                                      
 
-recoveryPasswordController.verifyCode = async (req,res) => {
+recoveryPasswordController.verifyCode = async (req, res) => {
     try {
-        //#1 solicitad codigo
-        const {code} = req.body;
-
-        //obtenemos la info del token
-        //accedenis a la cookie
-        const token = reportError.cookie.recoveryCookie;
-
-        const decoded = JsonWebtoken.verify(token, config.JWT.secret)
-
-        //ahora comparamos el codigo que escribio el usuario
-        //con el que esta dentro del token
-        if(code !== decoded.randomCode){
-            return res.status(400).json({message: "Invalid code"})
-        }
-
-        ///en cambio si escribe bien el codigo
-        //vamos a colocar en el token esta verificado
-        const newToken = JsonWebtoken.sign(
-            //#1 que guardaremos
-            {email: decoded.email, userType: "customer", verified: true},
-            //#2 clave secret
-            config.JWT.secret,
-            //#3 cuando expora
-            {expiresIn: "15m"},
-        );
-
-        res.cookies("recoveryCookie", newToken, {maxAge: 15 *60 * 1000});
-
-        return res.status(200).json({message: "code verified successfully"});
-
+      const { code } = req.body;
+      //obtenemos la informacion que esta dentro del token, accedemos a la cookie
+      const token = req.cookies.recoveryCookie;
+      const decoded = jsonwebtoken.verify(token, config.JWT.secret);
+      //comparamos los codigos
+      if (code !== decoded.randomCode) {
+        return res.status(400).json({ message: "Invalid code" });
+      }
+      //si lo escribe bien, saldra omg si beba
+      const newToken = jsonwebtoken.sign(
+        { email: decoded.email, userType: "customer", verified: true },
+        config.JWT.secret,
+        { expiresIn: "15m" },
+      );
+ 
+      res.cookie("recoveryCookie", newToken, { maxAge: 15 * 60 * 1000 });
+      return res.status(200).json({ message: "Code verified successfully" });
     } catch (error) {
-        console.log("error"+error)
-        return res.status(500).json({message: "INTERNAL SERVER ERROR"})
+      console.log("error" + error);
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-};
+  };
 
-recoveryPasswordController.newPassword = async (req,res) => {
-    try {
-        //#1 solicitar datos
-        const {newPassword, confirmNewPassword} = req.body;
-
-        //comparolos dos contraseñas
-        if(newPassword !== confirmNewPassword){
-            return res.status(400).json({message: "pasword doesnt match"})
-        }
-
-        //vamos omprobar que este verificado
-        //ya este en true o sea que haya paasdo el apso 2
-        const token = req.cookie.recoveryCookie;
-        const decoded = JsonWebtoken.verify(token, config.JWT.secret);
-
-        if(!decoded.verified){
-            return res.status(400).json({message: "code not verified"})
-        }
-
-        //actualiozar y encriptar
-        const passwordHash = await bcrypt.hash(newPassword, 10)
-
-        ///actualizar en la base de datos
-        await customerModel.findByIdAndUpdate(
-            {email: decoded.email},
-            {password: passwordHash},
-            {new: true},
-        );
+recoveryPasswordController.newPassword = async (req, res) => {
+  try {
+    const { newPassword, confirmNewPassword } = req.body;
+    if (newPassword !== confirmNewPassword) {
+      return res.status(400).json({ message: "password doesnt match" });
+    }
+    const token = req.cookies.recoveryCookie;
+    const decoded = jsonwebtoken.verify(token, config.JWT.secret);
+    if (!decoded.verified) {
+      return res.status(400).json({ message: "Code not verified" });
+    }
+    //Encriptar
+    const passwordHash = await bcrypt.hash(newPassword, 10);
+    //actualizar contraseña en la base de datos
+    await customerModel.findOneAndUpdate(
+      { email: decoded.email },
+      { password: passwordHash },
+      { new: true },
+    );
 
         res.clearCookie("recoveryCookie");
 
